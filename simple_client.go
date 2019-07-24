dd71e09b05e0d6476d598427dca61231402de9a1
cedarren <cedarren@tencent.com>
2019-07-24 01:03:24 -0700

    1000 client press test

--- simple_client.go
+++ simple_client.go
@@ -5,21 +5,34 @@ import (
 	"net"
 )
 
-func main() {
-
-	fmt.Println("client start!")
-
+func establishConn(i int) net.Conn {
 	conn, err := net.Dial("tcp", ":8899")
 	if nil != err {
 		fmt.Println(err)
-		return
+		return nil
 	}
 
-	fmt.Println("Connect to localhost:8899 success")
+	fmt.Printf("[%d] Connect to localhost:8899 success\r\n", i)
 	defer conn.Close()
 
 	str := "hello,world"
 	conn.Write([]byte(str))
+
+	return conn
+}
+
+func main() {
+
+	var s1 []net.Conn
+
+	fmt.Println("client start!")
+
+	for i := 0; i < 1000; i++ {
+		conn := establishConn(i)
+		if nil != conn {
+			s1 = append(s1, conn)
+		}
+	}
 
 	fmt.Println("client finish")
 
